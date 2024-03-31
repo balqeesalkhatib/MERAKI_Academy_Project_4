23af839dd2e5347b13b9e7436306ba1711ede457
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-03-31 15:56:16 +0300

    ReadByCategoryId

--- Project 4 Starter Code/backend/controllers/product.js
+++ Project 4 Starter Code/backend/controllers/product.js
@@ -1,4 +1,5 @@
 const productModel = require("../models/productSchema");
+//should send token
 const createProduct = (req, res) => {
   //by user
   const { name, description, price, category, image } = req.body;
@@ -29,4 +30,31 @@ const createProduct = (req, res) => {
     });
   })
 };
-module.exports = { createProduct };
+const readByCategoryId=(req,res)=>{
+const {id}=req.params
+productModel.find({category:id}).populate('category').exec()
+.then((result) => {
+   if(result.length){
+    res.status(201).json({
+        success: true,
+        message: `Products of category ${id}`,
+        result: result,
+      });
+   }
+   else{
+    res.status(404).json({
+        success: false,
+        message: `The category with ${id} has no products yet`,
+        result: result,
+      });
+   }
+  })
+.catch((err) => {
+    res.status(500).json({
+      success: false,
+      message: `Server error`,
+      error: err.message,
+    });
+  })
+}
+module.exports = { createProduct,readByCategoryId };
--- Project 4 Starter Code/backend/routes/product.js
+++ Project 4 Starter Code/backend/routes/product.js
@@ -2,11 +2,12 @@ const express = require("express");
 const productRouter = express.Router();
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
-const { createProduct } = require("../controllers/product");
+const { createProduct, readByCategoryId } = require("../controllers/product");
 productRouter.post(
   "/:id/product",
   authentication,
   authorization("create"),
   createProduct
 );
+productRouter.get("/:id/product", authentication, readByCategoryId);
 module.exports = productRouter;
