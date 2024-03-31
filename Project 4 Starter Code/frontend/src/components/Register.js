5d6feb1d6ef84174f84b4fc81dcf78a3f54c080b
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-04-01 00:40:27 +0300

    frontend- register step 1

--- Project 4 Starter Code/frontend/src/components/Register.js
+++ Project 4 Starter Code/frontend/src/components/Register.js
@@ -1,16 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
 const Register = () => {
+  const [first, setFirst] = useState("");
+  const [Last, setLast] = useState("");
+  const [Country, setCountry] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
   return (
     <div>
-        <input placeholder='First Name'/>
-        <input placeholder='Last Name'/>
-        <input placeholder='Country'/>
-        <input placeholder='Email'/>
-        <input placeholder='Password'/>
-        <button>Register</button>
+      <input placeholder="First Name" type="text" onClick={(e)=>{
+setFirst(e.target.value)
+      }}/><br />
+      <input placeholder="Last Name" type="text"  onClick={(e)=>{
+        setLast(e.target.value)
+    }}/><br />
+      <input placeholder="Country" type="text"  onClick={(e)=>{
+        setCountry(e.target.value)
+    }}/><br />
+      <input placeholder="Email" type="email"  onClick={(e)=>{
+        setEmail(e.target.value)
+    }}/><br />
+      <input placeholder="Password" type="password"  onClick={(e)=>{
+        setPassword(e.target.value)
+    }}/><br />
+      <button>Register</button>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
