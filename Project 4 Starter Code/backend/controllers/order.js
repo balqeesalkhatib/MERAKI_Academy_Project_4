d3ba94e91dd007ef83e0c3b8256f514f5b7b2035
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-03-31 21:16:36 +0300

    ReadByUserId

--- Project 4 Starter Code/backend/controllers/order.js
+++ Project 4 Starter Code/backend/controllers/order.js
@@ -1,22 +1,50 @@
-const orderModel=require('../models/orderSchema')
+const orderModel = require("../models/orderSchema");
 // send token
-const createOrder=(req,res)=>{
-const {status,date,product,user}=req.body
-const newOrder= new orderModel({status,date,product,user});
-newOrder.save()
-.then((result)=>{
-    res.status(201).json({
+const createOrder = (req, res) => {
+  const { status, date, product, user } = req.body;
+  const newOrder = new orderModel({ status, date, product, user });
+  newOrder
+    .save()
+    .then((result) => {
+      res.status(201).json({
         success: true,
-      message: `Order has been created successfully`,
-      result:result,
+        message: `Order has been created successfully`,
+        result: result,
+      });
     })
-})
-.catch((err) => {
-    res.status(500).json({
-      success: false,
-      message: `Server error`,
-      error: err.message,
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        error: err.message,
+      });
     });
-  })
-}
-module.exports={createOrder}
+};
+const getOrder = (req, res) => {
+  const { id } = req.params;
+  orderModel
+    .find({user:id})
+    .then((result) => {
+      if (result.length) {
+        res.status(200).json({
+          success: true,
+          message: `All the orders for user ${id}`,
+          result: result,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: `User with ${id} has no order yet`,
+        
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server error`,
+        error: err.message,
+      });
+    });
+};
+module.exports = { createOrder, getOrder };
--- Project 4 Starter Code/backend/routes/order.js
+++ Project 4 Starter Code/backend/routes/order.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const orderRouter=express.Router();
-const {createOrder}=require('../controllers/order')
+const {createOrder,getOrder}=require('../controllers/order')
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
 orderRouter.post('/', 
 authentication,
 authorization("create"),
 createOrder)
+orderRouter.get('/:id',getOrder)
 module.exports=orderRouter
